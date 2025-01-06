a0381c2d5c01d5cdd733da7231087c9b77b70e5f
Shahin-Zeyni <zeynishahinn@gmail.com>
2025-01-06 20:20:30 +0330

    Update TypeScript.ts

--- TypeScript.ts
+++ TypeScript.ts
@@ -15,4 +15,43 @@ interface ChildProps {
   isOpen: boolean;
   setIsOpen: (value: boolean) => void;
 }
+
+
+#ٌ Way 1
+const DeleteServicesBookingButton = ({ id }: { id: number }) => {
+--------------------------------
+# Way 2
+import { FC, ReactNode } from 'react';
+
+interface BaseModalProps {
+  opened: boolean;
+  onClose: () => void;
+  children: ReactNode;
+  size?: string | number;
+  title: string;
+  withCloseButton: boolean;
+}
+
+const BaseModal: FC<BaseModalProps> = ({
+  opened,
+  onClose,
+  children,
+  size = 1016,
+  title,
+  ...restOfProps
+}) => (
+  <Modal
+    opened={opened}
+    onClose={onClose}
+    title={title}
+    size={size}
+    centered
+    {...restOfProps}
+  >
+    {children}
+  </Modal>
+);
+
+export default BaseModal;
+
 ```
